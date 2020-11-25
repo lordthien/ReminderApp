b188599b09b3e38142a224ed0b06023cdb781d2e
lordthien <nnthien.242@gmail.com>
2020-11-25 14:09:07 +0700

    sprint 3

---
+++ app/src/main/java/com/example/ngocthien/remindertj/About/Text.java
@@ -0,0 +1,13 @@
+package com.example.ngocthien.remindertj.About;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(AboutActivity.activity_text);
+    }
+}
